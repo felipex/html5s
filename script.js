167927a70bbaa5dc914027c51770c3e45d4c88a4
Glitch (glitch-hello-website) <none>
2023-03-06 01:40:18 +0000

    ⛳️🎸 Checkpoint ./script.js:5277657/1665 ./style.css:5277657/58

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ let index = 0;
 const slides = document.getElementById("slides");
 
 function selectSlide(index = 0){
-    
+  for (let i = 0; i < slides.children.length; i++) {
+    slides.children.item(i).classList.add("back");
+  }
+  slides.children.item(index).classList.remove("back");
+  slides.children.item(index).classList.add("front");
 }
 
 function keyEvent(event) {
-  console.log(event);
-  console.log(slides);
+  if (event.key == "ArrowRight") {
+    index++;
+  }
+  if (event.key == "ArrowLeft") {
+    index--;
+  }
+  selectSlide(index);
 }
 
-console.log(slides);
+console.log(slides.style.left);
 document.body.addEventListener("keydown", keyEvent);
-
+selectSlide(0);
 
