96ec5703ae3cbcf8a4e2f95ee4445b0716f74d87
Glitch (glitch-hello-website) <none>
2023-03-07 01:09:30 +0000

    ⛩🎐 Checkpoint ./script.js:5277657/10697

--- script.js
+++ script.js
@@ -32,22 +32,22 @@ document.body.addEventListener("keydown", keyEvent);
 selectSlide(0);
 
 function loadRemoteSlide(slide, file) {
-  fetch(`/slides/${file}`).then(response => {
-    if (response.ok) {
-      return response.text();
-    }
-  })
-  .then(txt => {
-    slide.innerHTML = txt;
-  })
+  fetch(`/slides/${file}`)
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      }
+    })
+    .then((txt) => {
+      slide.innerHTML = txt;
+    });
 }
 
 function loadRemoteSlides() {
   for (let i = 0; i < slides.children.length; i++) {
     const slide = slides.children.item(i);
     if (slide.dataset.filename) {
-      let slideID = slide.dataset.filename;
-      let slideFile = `${slideID}.html`;
+      let slideFile = `${slide.dataset.filename}.html`;
       loadRemoteSlide(slide, slideFile);
     }
   }
