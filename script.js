64cda68d226e39693323a231d0f57eb8f34dfb10
Glitch (glitch-hello-website) <none>
2023-04-10 11:57:26 +0000

    🕷🙏 Checkpoint ./index.html:5277657/494 ./hud.css:5277657/264 ./script.js:5277657/13398

--- script.js
+++ script.js
@@ -29,6 +29,8 @@ function keyEvent(event) {
 const header = document.getElementById("header");
 
 document.body.addEventListener("keydown", keyEvent);
+document.body.getElementsByClassName("key").add
+
 selectSlide(0);
 
 function loadRemoteSlide(slide, file) {
@@ -55,53 +57,49 @@ function loadRemoteSlides() {
 
 loadRemoteSlides();
 
-
-function marcaColuna(e, col){
+function marcaColuna(e, col) {
   const coluna = document.getElementsByClassName(col);
 
   for (let i = 0; i < coluna.length; i++) {
     const celula = coluna.item(i);
     celula.classList.add("marcaCol");
-  }  
+  }
 }
 
-function desmarcaColuna(e, col){
+function desmarcaColuna(e, col) {
   const coluna = document.getElementsByClassName(col);
 
   for (let i = 0; i < coluna.length; i++) {
     const celula = coluna.item(i);
     celula.classList.remove("marcaCol");
-  }  
+  }
 }
 
-
 function clicou(e) {
-  
   const cell = e.target;
- if (cell.textContent) {
-   let nivel = "";
-   if (cell.classList.contains("col4")) {
-     nivel = "1";
-   } else if (cell.classList.contains("col5")) {
-     nivel = "2";
-   } else if (cell.classList.contains("col6")) {
-     nivel = "3";
-   } else if (cell.classList.contains("col7")) {
-     nivel = "4";
-   } 
-   const out = document.getElementsByClassName("padraoDeVenvimento")[0];
-   const step = cell.textContent.padStart(2, "0");
-   out.innerHTML = `<span class="nivel">D</span><span class="capac">${nivel}</span><span class="padrao">${step}</span>`;
+  if (cell.textContent) {
+    let nivel = "";
+    if (cell.classList.contains("col4")) {
+      nivel = "1";
+    } else if (cell.classList.contains("col5")) {
+      nivel = "2";
+    } else if (cell.classList.contains("col6")) {
+      nivel = "3";
+    } else if (cell.classList.contains("col7")) {
+      nivel = "4";
+    }
+    const out = document.getElementsByClassName("padraoDeVenvimento")[0];
+    const step = cell.textContent.padStart(2, "0");
+    out.innerHTML = `<span class="nivel">D</span><span class="capac">${nivel}</span><span class="padrao">${step}</span>`;
     console.log(out.innerHTML);
   }
 }
 
-function marcaCliques(){
+function marcaCliques() {
   const table = document.getElementsByTagName("table")[0];
   const tds = document.getElementsByTagName("td");
   for (let i = 0; i < tds.length; i++) {
     const celula = tds.item(i);
-    celula.addEventListener("click", clicou)
-  }  
-  
+    celula.addEventListener("click", clicou);
+  }
 }
