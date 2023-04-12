d45f2d7833fab95b4b0e1a5d825bef92ff986ddd
Glitch (glitch-hello-website) <none>
2023-04-12 00:25:30 +0000

    🐟🐽 Checkpoint ./load_slides.js:5277657/1286 ./hud.js:5277657/527 ./index.html:5277657/346

--- hud.js
+++ hud.js
@@ -31,29 +31,5 @@ function keyEvent(event) {
 
 document.body.addEventListener("keydown", keyEvent);
 
-function loadRemoteSlide(slide, file) {
-  fetch(`/slides/${file}`)
-    .then((response) => {
-      if (response.ok) {
-        return response.text();
-      }
-    })
-    .then((txt) => {
-      slide.innerHTML = txt;
-    });
-}
-
-function loadRemoteSlides() {
-  for (let i = 0; i < slides.children.length; i++) {
-    const slide = slides.children.item(i);
-    if (slide.dataset.filename) {
-      let slideFile = `${slide.dataset.filename}.html`;
-      loadRemoteSlide(slide, slideFile);
-    }
-  }
-}
-
-loadRemoteSlides();
-
 // Show the first slide.
 showSlide(0);
